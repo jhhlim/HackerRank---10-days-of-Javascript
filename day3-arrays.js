fb7c2545dd5600900c35991c2cd58716cdde2602
jhhlim <31980053+jhhlim@users.noreply.github.com>
2021-12-24 18:19:28 -0800

    Update day3-arrays.js

--- day3-arrays.js
+++ day3-arrays.js
@@ -1,26 +1,4 @@
-'use strict';
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
-    
-    main();    
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
 
 /**
 *   Return the second largest number in the array.
@@ -29,12 +7,22 @@ function readLine() {
 **/
 function getSecondLargest(nums) {
     // Complete the function
+
+    //  var sArray = nums.sort(function (a,b){
+    //     return a - b;
+    // });
+    // var uSarray = sArray.filter(function (elm, index, self){
+    //     return index == self.indexOf(elm);
+    // });
+    // return uSarray[uSarray.length - 2];
+    var max = Math.max.apply(null, nums);
+
+    function delmax(num) {
+     return num < max;
+    }
+    const nums2 = nums.filter(delmax);
+
+    var max2 = Math.max.apply(null, nums2);
+    return max2;
 }
 
-
-function main() {
-    const n = +(readLine());
-    const nums = readLine().split(' ').map(Number);
-    
-    console.log(getSecondLargest(nums));
-}
