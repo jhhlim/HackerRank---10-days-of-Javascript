3fa43ba0a0cd6774e0835eb229341b240866017f
jhhlim <31980053+jhhlim@users.noreply.github.com>
2021-12-24 18:16:59 -0800

    Update day1-arithmetic-ops.js

--- day1-arithmetic-ops.js
+++ day1-arithmetic-ops.js
@@ -1,26 +1,4 @@
-'use strict';
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
-    
-    main();    
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
 
 /**
 *   Calculate the area of a rectangle.
@@ -33,7 +11,7 @@ function readLine() {
 function getArea(length, width) {
     let area;
     // Write your code here
-    
+    area = length*width;
     return area;
 }
 
@@ -48,7 +26,7 @@ function getArea(length, width) {
 function getPerimeter(length, width) {
     let perimeter;
     // Write your code here
-    
+    perimeter = (length+width)*2;
     return perimeter;
 }
 
