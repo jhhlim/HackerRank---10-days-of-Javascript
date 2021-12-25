71c6c5e38ff21b64a77b0482a8d143e6c069eaf9
jhhlim <31980053+jhhlim@users.noreply.github.com>
2021-12-24 18:19:11 -0800

    Update day2-loops.js

--- day2-loops.js
+++ day2-loops.js
@@ -1,38 +1,36 @@
-'use strict';
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
-    
-    main();    
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
 
 /*
  * Complete the vowelsAndConsonants function.
  * Print your output using 'console.log()'.
  */
 function vowelsAndConsonants(s) {
-    
+    var strConsonants = "";
+    var strVowels = "";
+    var i;
+
+for (i in s) {
+    if (s.charAt(i) == "a" || s.charAt(i) == "e" || s.charAt(i) == "i" || 
+s.charAt(i) == "o" || s.charAt(i) == "u") {
+        strVowels += s.charAt(i);
+    }
+    else {
+        strConsonants += s.charAt(i);
+    }
+}
+
+i = 0;
+for (i in strVowels) {
+    console.log(strVowels.charAt(i));
+}
+
+// console.log(strConsonants);
+
+i = 0;
+for (i in strConsonants) {
+     console.log(strConsonants.charAt(i));
 }
 
 
-function main() {
-    const s = readLine();
-    
-    vowelsAndConsonants(s);
 }
+
