8d7a2e019d11cef57e14585b84582a9772d0e047
jhhlim <31980053+jhhlim@users.noreply.github.com>
2021-12-31 00:15:34 -0800

    Create day7-regular-expressions.js

---
+++ day7-regular-expressions.js
@@ -0,0 +1,32 @@
+
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+     */
+
+    let re = new RegExp(/^([aeiou]).*\1$/);
+       return re;
+       
+       
+//https://stackoverflow.com/questions/8327705/what-are-and-in-regular-expressions
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+
+function test(re)
+{
+    if (re[0] == re.match(/[aeoiu]/)  && re[length] == re.match(/[aeoiu]/))
+    {
+        re = true;
+    } 
+    else
+    {
+        re = false;
+    }
+    return re;
+}
+
